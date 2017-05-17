5fef07a8a28f200ff50dab3d85667f544042e040
HeShulin <heshulin@imudges.com>
2017-05-17 19:46:02 +0800

    bean完成

---
+++ src/com/iheshulin/yourname/util/MD5.java
@@ -0,0 +1,41 @@
+package com.iheshulin.yourname.util;
+
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+/**
+ * Created by HeShulin on 2017/5/17.
+ */
+public class MD5 {
+    public static String getMd5(String plainText) {
+        StringBuffer buf = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(plainText.getBytes());
+            byte b[] = md.digest();
+            int i;
+            buf = new StringBuffer("");
+            for (int offset = 0; offset < b.length; offset++) {
+                i = b[offset];
+                if (i < 0)
+                    i += 256;
+                if (i < 16)
+                    buf.append("0");
+                buf.append(Integer.toHexString(i));
+            }
+
+
+
+        } catch (NoSuchAlgorithmException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return buf.toString();
+    }
+
+    private static final String salt = "heshulin@imudges.com";
+
+    public static String encryptTimeStamp(String ts){
+        return getMd5(getMd5(ts)+"&"+salt);
+    }
+}
